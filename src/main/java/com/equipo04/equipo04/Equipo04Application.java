1bb55096e9012b9a47a5f99f0984bc68843c947b
Mauricio <halconrn1@yahoo.es>
2023-08-04 18:37:20 -0500

    Creación Repo

---
+++ src/main/java/com/equipo04/equipo04/Equipo04Application.java
@@ -0,0 +1,13 @@
+package com.equipo04.equipo04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo04/equipo04/Equipo04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo04.equipo04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
