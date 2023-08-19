636a3f4bd90cebd7ad61cd077fc8bb2e2c9cf624
GonzaloGollo <gonzalogollo@gmail.com>
2023-08-19 01:08:16 -0300

    ajustes cards en front

--- frontend/03/project-test/src/Components/ListadoDeProductos/CardProducto.jsx
+++ frontend/03/project-test/src/Components/ListadoDeProductos/CardProducto.jsx
@@ -17,20 +17,17 @@ const CardProducto = ({
   return (
     <div className="card-body-producto">
       <Link to={"/producto/" + id}>
-        <img className="ImgCard-producto" src={url} alt="Foto producto" />
+        {/* <div  style={{
+        backgroundImage: `url(${url})`,
+        backgroundSize: "cover",
+      }}
+></div> */}
+        <img className="ImgCard-producto-home" src={url} alt="Foto producto" />
         <div className="textos-card">
           <div className="titulo-descripcion-tipo-servicios">
             <h3 className="titulo-card-producto">{title}</h3>
             <div className="descripcion-tipo">
               <p className="descripcion-card-home p-sinmargin">{descripcion}</p>
-              {/* <p className="tipo-recurso-card p-sinmargin">{tipoRecurso}</p> */}
-              {/* <div className="servicios">
-                <p className="servicio-card p-sinmargin">{servicio1}</p>
-                <p className="p-sinmargin">-</p>
-                <p className="servicio-card p-sinmargin">{servicio2}</p>
-                <p className="p-sinmargin">-</p>
-                <p className="servicio-card p-sinmargin">{servicio3}</p>
-              </div> */}
             </div>
           </div>
 
@@ -38,13 +35,12 @@ const CardProducto = ({
             <p className="precio-card-home">${precio}</p>
           </div>
         </div>
-
-       
       </Link>
-    
     </div>
   );
 };
+
+
 
 CardProducto.propTypes = {
   title: PropTypes.string.isRequired,
