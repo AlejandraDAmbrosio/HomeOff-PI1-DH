a82914de4e4d346533bdd9e589d53bf3b2e477ee
GonzaloGollo <gonzalogollo@gmail.com>
2023-09-06 21:48:50 -0300

    Nueva Fx de Iniciales en comentarios

--- frontend/03/project-test/src/Components/Navbar/AvatarNav.jsx
+++ frontend/03/project-test/src/Components/Navbar/AvatarNav.jsx
@@ -7,7 +7,7 @@ import Avatar from "@mui/joy/Avatar";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
-const AvatarNav = () => {
+const AvatarNav = ({Iniciales}) => {
   const { usuarioLogueado, iniciarSesion, cerrarSesion } =
     useContext(ContextGlobal);
 
@@ -22,8 +22,8 @@ const AvatarNav = () => {
             backgroundColor: "#9dd6b3",
             color: "black",
           }}
-        >
-          {obtenerIniciales(usuarioLogueado.nombreCompleto)}
+        > {Iniciales ? (obtenerIniciales(Iniciales)) :(obtenerIniciales(usuarioLogueado.nombreCompleto)) }
+          
         </Avatar>
       ) : (
         <Avatar
