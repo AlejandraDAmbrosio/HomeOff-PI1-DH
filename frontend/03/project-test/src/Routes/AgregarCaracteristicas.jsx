5c879e430c727b39344e81356069a2ebfa6ba7d4
GonzaloGollo <gonzalogollo@gmail.com>
2023-09-13 16:18:11 -0300

    envio de caracteristicas en funcionamiento feo pero funciona

--- frontend/03/project-test/src/Routes/AgregarCaracteristicas.jsx
+++ frontend/03/project-test/src/Routes/AgregarCaracteristicas.jsx
@@ -44,8 +44,9 @@ const AgregarCaracteristicas = () => {
 
   const [form, setForm] = useState(false);
   const [error, setError] = useState(null);
-  const [caracteristicasSeleccionadas, setCaracteristicasSeleccionadas] =
-    useState({});
+
+  const [caracteristicasSeleccionadas, setCaracteristicasSeleccionadas] = useState([]);
+
 
   /////// Preparar objeto para enviar al servidor    ///////
 
@@ -63,29 +64,35 @@ const AgregarCaracteristicas = () => {
 
   //////////////////OnChanges///////////////
 
-  const handleOptionChange = (caracteristica) => {
-    setCaracteristicasSeleccionadas((prevCaracteristicas) => ({
-      ...prevCaracteristicas,
-      [caracteristica.idCaracteristica]:
-        !prevCaracteristicas[caracteristica.idCaracteristica],
-    }));
-  };
+// const handleOptionChange = (caracteristica) => {
+//   if (caracteristicasSeleccionadas.some((item) => item.idCaracteristica === caracteristica.idCaracteristica)) {
+//     // Si ya está seleccionado, quítalo del array
+//     setCaracteristicasSeleccionadas((prevCaracteristicas) => 
+//       prevCaracteristicas.filter((item) => item.idCaracteristica !== caracteristica.idCaracteristica)
+//     );
+//   } else {
+//     // Si no está seleccionado, agrégalo al array
+//     setCaracteristicasSeleccionadas((prevCaracteristicas) => 
+//       [...prevCaracteristicas, caracteristica]
+//     );
+//   }
+// };
 
   /////////handleSubmit //////
-  const handleSubmitCrearProducto = async (e) => {
-    e.preventDefault();
+  // const handleSubmitCrearProducto = async (e) => {
+  //   e.preventDefault();
 
-    if (formCaracteristicas != null) {
-      console.log("----Info paquete enviado en nuevoProductoData---");
-      console.log(formCaracteristicas);
+  //   if (formCaracteristicas != null) {
+  //     console.log("----Info paquete enviado en nuevoProductoData---");
+  //     console.log(formCaracteristicas);
 
-      console.log("------------------formCaracteristicas  ------------------");
-      console.log(formCaracteristicas);
+  //     console.log("------------------formCaracteristicas  ------------------");
+  //     console.log(formCaracteristicas);
 
       //////// Envio de info a Caracteristicas///////////////////
 
-      const urlCaracteristicasXIdRec =
-        "http://52.32.210.155:8080/auth/inter/save";
+      // const urlCaracteristicasXIdRec =
+      //   "http://52.32.210.155:8080/auth/inter/save";
 
       // const caracteristicasParaEnviar = Object.keys(
       //   caracteristicasSeleccionadas
@@ -96,16 +103,23 @@ const AgregarCaracteristicas = () => {
       // }));
 
 
-      const caracteristicasParaEnviar = {
-        idCaracteristica: 10, 
-        idRecurso: 229, 
-        idCaracteristicas_x_Recurso: 0, 
-      };
+      // const caracteristicasParaEnviar = {
+      //   idCaracteristica: 10, 
+      //   idRecurso: 229, 
+      //   idCaracteristicas_x_Recurso: 0, 
+      // };
 
-      console.log(
-        "----caracteristicasParaEnviar: --->",
-        caracteristicasParaEnviar
-      );
+
+      // const caracteristicasParaEnviar = caracteristicasSeleccionadas.map((caracteristica) => ({
+      //   idCaracteristica: caracteristica.idCaracteristica,
+      //   idRecurso: recursoXID.idRecurso,
+      //   idCaracteristicas_x_Recurso: 0,
+      // }));
+
+      // console.log(
+      //   "----caracteristicasParaEnviar: --->",
+      //   caracteristicasParaEnviar
+      // );
 
       // try {
       //   console.log("segundo Try: ------------------------------------------------>");
@@ -139,132 +153,161 @@ const AgregarCaracteristicas = () => {
       //     setError("Error en la segunda llamada a axios.post: " + error.message);
       //   }
 
-      try {
-        console.log(
-          "segundo Try: ------------------------------------------------>"
-        );
+  //     try {
+  //       console.log(
+  //         "segundo Try: ------------------------------------------------>"
+  //       );
 
-        const jsonData2 = JSON.stringify(caracteristicasParaEnviar);
+  //       const jsonData2 = JSON.stringify(caracteristicasParaEnviar);
 
-        console.log("jsonData2", jsonData2);
-        console.log("caracteristicasParaEnviar", caracteristicasParaEnviar);
+  //       console.log("jsonData2", jsonData2);
+  //       console.log("caracteristicasParaEnviar", caracteristicasParaEnviar);
 
-        const response2 = await fetch(urlCaracteristicasXIdRec, {
-          method: "POST",
+  //       const response2 = await fetch(urlCaracteristicasXIdRec, {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: jsonData2,
+  //       });
+
+  //       if (response2.status === 200) {
+  //         const responseData2 = await response2.json();
+  //         console.log("Respuesta:", responseData2);
+  //       } else {
+  //         console.error(
+  //           "Error en la segunda llamada a fetch:",
+  //           response2.status,
+  //           response2.statusText
+  //         );
+  //         setError(
+  //           "Error en la segunda llamada a fetch: " + response2.statusText
+  //         );
+  //       }
+  //     } catch (error) {
+  //       console.error("Error en la segunda llamada a axios.post:", error);
+  //       setError(
+  //         "Error en la llamada a axios.post. Fallo el intento: " + error.message
+  //       );
+  //     }
+
+  //     /////ERROR ????////////////////////////
+  //   } else {
+  //     setForm(false);
+  //   }
+  // };
+
+  const handleOptionChange = async (caracteristica) => {
+    try {
+      const urlCaracteristicasXIdRec =
+        "http://52.32.210.155:8080/auth/inter/save";
+
+      const caracteristicasParaEnviar = {
+        idCaracteristica: caracteristica.idCaracteristica,
+        idRecurso: recursoXID.idRecurso,
+        idCaracteristicas_x_Recurso: 0,
+      };
+
+      const jsonData = JSON.stringify(caracteristicasParaEnviar);
+
+      const response = await axios.post(
+        urlCaracteristicasXIdRec,
+        jsonData,
+        {
           headers: {
             "Content-Type": "application/json",
           },
-          body: jsonData2,
-        });
-
-        if (response2.status === 200) {
-          const responseData2 = await response2.json();
-          console.log("Respuesta:", responseData2);
-        } else {
-          console.error(
-            "Error en la segunda llamada a fetch:",
-            response2.status,
-            response2.statusText
-          );
-          setError(
-            "Error en la segunda llamada a fetch: " + response2.statusText
-          );
         }
-      } catch (error) {
-        console.error("Error en la segunda llamada a axios.post:", error);
-        setError(
-          "Error en la llamada a axios.post. Fallo el intento: " + error.message
-        );
-      }
+      );
 
-      /////ERROR ????////////////////////////
-    } else {
-      setForm(false);
+      if (response.status === 200) {
+        const responseData = await response.data;
+        console.log("Respuesta:", responseData);
+      } else {
+        console.error(
+          "Error en la llamada a axios.post:",
+          response.status,
+          response.statusText
+        );
+        setError("Error en la llamada a axios.post: " + response.statusText);
+      }
+    } catch (error) {
+      console.error("Error en la llamada a axios.post:", error);
+      setError(
+        "Error en la llamada a axios.post. Fallo el intento: " + error.message
+      );
     }
   };
 
+
   return (
     <div className="administracion-agre" style={{ padding: "2rem 0rem" }}>
-      <div
-        className="administracion-agre-titulo"
-        style={{ padding: "0rem 0rem" }}
-      >
-        Editar características por producto
-      </div>
-      <div className="paneles-agregar">
-        <PanelAdminUser />
-        <div className="division-form-preview">
-          <div className="pagina-formulario-alta-producto">
-            <form onSubmit={handleSubmitCrearProducto}>
-              <FormControl
-                // onSubmit={handleSubmitCrearProducto}
-                style={{ padding: "1rem 0rem", width: "500px" }}
+    <div
+      className="administracion-agre-titulo"
+      style={{ padding: "0rem 0rem" }}
+    >
+      Editar características por producto
+    </div>
+    <div className="paneles-agregar">
+      <PanelAdminUser />
+      <div className="division-form-preview">
+        <div className="pagina-formulario-alta-producto">
+          <FormControl
+            style={{ padding: "1rem 0rem", width: "500px" }}
+          >
+            <div className="formularioAgregarProducto">
+              <FormGroup
+                className="formgroup-check-boxs"
+                label="Elija las características"
+                component="fieldset"
+                style={{ maxWidth: "480px", height: "fit-content" }}
               >
-                <div className="formularioAgregarProducto">
-                  <FormGroup
-                    className="formgroup-check-boxs"
-                    label="Elija las caracteristicas"
-                    component="fieldset"
-                    style={{ maxWidth: "480px", height: "fit-content" }}
-                  >
-                    <FormLabel component="legend">Características</FormLabel>
-                    <div className="container-check-boxs">
-                      {caracteristicasLista.map((caracteristica) => (
-                        <li
-                          key={caracteristica.idCaracteristica}
-                          style={{ listStyle: "none" }}
-                          className="item-grid-check"
-                        >
-                          <label>
-                            <Checkbox
-                              type="checkbox"
-                              // className="item-grid-check"
-                              checked={caracteristica.checked} // Asumo que cada objeto tiene una propiedad "checked"
-                              onChange={() =>
-                                handleOptionChange(caracteristica)
-                              }
-                            />
-                            {caracteristica.nombre}
-                          </label>
-                        </li>
-                      ))}
-                    </div>
-                  </FormGroup>
-                  {/* /////////--------------------------------////// */}
+                <FormLabel component="legend">Características</FormLabel>
+                <div className="container-check-boxs">
+                  {caracteristicasLista.map((caracteristica) => (
+                    <li
+                      key={caracteristica.idCaracteristica}
+                      style={{ listStyle: "none" }}
+                      className="item-grid-check"
+                    >
+                      <label>
+                        <Checkbox
+                          type="checkbox"
+                          checked={caracteristica.checked}
+                          onChange={() =>
+                            handleOptionChange(caracteristica)
+                          }
+                        />
+                        {caracteristica.nombre}
+                      </label>
+                    </li>
+                  ))}
                 </div>
-                {form && (
-                  <h5 className="msj-form-guardado">
-                    Gracias !! Tu producto ha sido guardado!
-                  </h5>
-                )}
-                {error && <div className="error-message">{error}</div>}
+              </FormGroup>
+            </div>
+            {form && (
+              <h5 className="msj-form-guardado">
+                Gracias !! Tu producto ha sido guardado!
+              </h5>
+            )}
+            {error && <div className="error-message">{error}</div>}
 
-                <div className="boton-acceso-agregar-producto">
-                  <Button
-                    className="boton"
-                    type="submit"
-                    variant="outlined"
-                    onClick={handleSubmitCrearProducto}
-                  >
-                    Guardar
-                  </Button>
-
-                  <Button
-                    className="boton"
-                    type="reset"
-                    variant="outlined"
-                    color="error"
-                  >
-                    Cancelar
-                  </Button>
-                </div>
-              </FormControl>
-            </form>
-          </div>
+            <div className="boton-acceso-agregar-producto">
+              <Button
+                className="boton"
+                type="reset"
+                variant="outlined"
+                color="error"
+              >
+                Cancelar
+              </Button>
+            </div>
+          </FormControl>
         </div>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default AgregarCaracteristicas;
